7dd50d5ca59f18f617f0a583d3ed799d02a54231
Zen Li <zenli@outlook.com>
2024-03-05 13:06:32 +0800

    README

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 module.exports = function (req) {
     let ip = req.headers['x-real-ip']
           || req.headers['x-forwarded-for']
-          || req.connection.remoteAddress
-          || req.socket.remoteAddress
-          || req.connection.socket.remoteAddress
+          || req.connection?.remoteAddress
+          || req.socket?.remoteAddress
+          || req.connection?.socket?.remoteAddress
 
     if (ip.indexOf(':') > -1 && ip.indexOf('.') > 0) {
         ip = ip.replace('::ffff:', '')
